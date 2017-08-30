89e9a3f399816d4600f5806a94382246500dc882
Sara Hopper <sara.hopper@gmail.com>
2017-08-30 18:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9000,3 +9000,13 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+const issuesWithUpdatedApiUrl = issues.map(issue =>
+  Object.assign({}, issue, {
+    url: issue.url.replace('api.github.com', 'api-v2.github.com')
+  }));
+
+const commentCountAcrossIssues = issues
+  .map(issue => issue.comments_count)
+  .reduce((total, count) => total + count, 0);
+)
